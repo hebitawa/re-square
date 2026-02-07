51b67d818a8b1f8ac233b786e0c4980f1ecd584b
hebitawa <hebino.tawagoto@gmail.com>
2026-02-07 13:22:15 +0900

    Create keymap.c

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include QMK_KEYBOARD_H
+#include "keymap_japanese.h"
+#include "print.h"
+#include "lib_trackball.h"
+#include "lib_led.h"
+
+#define ____    KC_TRNS
+#define XXXX    KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(
+		KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+		KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,
+		KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_TRNS, KC_TRNS,
+		KC_UP,     MO(3),   KC_LALT, KC_SPC,  XXXX,    KC_LEFT, KC_DOWN, KC_RGHT, LT(2, JP_MHEN),
+		MO(3),     XXXX,    LT(1, JP_HENK),   KC_ENT,  KC_RGHT, KC_UP,   KC_LEFT, KC_RGUI, KC_DOWN
+	),
+	[1] = LAYOUT(
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____
+	),
+	[2] = LAYOUT(
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____
+	),
+	[3] = LAYOUT(
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____
+	)
+};
+
+
+void keyboard_post_init_user(void) {
+    trackball_post_init();
+    led_post_init();
+}
