1b7577b61253887f01248ef19197026e5be5a2e6
hebitawa <hebino.tawagoto@gmail.com>
2026-03-20 11:03:43 +0900

    Add files via upload

--- keymaps/vial/library/lib_trackball.c
+++ keymaps/vial/library/lib_trackball.c
@@ -8,12 +8,18 @@
 #define PRECISION_CPI 800
 
 bool set_scrolling = false;
-
-/* 左右役割反転フラグ */
 bool swap_trackball = false;
+bool dual_cursor_mode = false;
 
+/* 通常スクロール */
 float scroll_accumulated_h = 0;
 float scroll_accumulated_v = 0;
+
+/* Wスクロール */
+float scroll_accumulated_h_l = 0;
+float scroll_accumulated_v_l = 0;
+float scroll_accumulated_h_r = 0;
+float scroll_accumulated_v_r = 0;
 
 
 /* ---------- CPI設定 ---------- */
@@ -34,14 +40,10 @@ static uint8_t cursor_cpi_index = 2;
 void trackball_post_init(void) {
 
 #ifdef POINTING_DEVICE_COMBINED
-
     pointing_device_set_cpi_on_side(true,  cursor_cpi_list[cursor_cpi_index]);
     pointing_device_set_cpi_on_side(false, cursor_cpi_list[cursor_cpi_index]);
-
 #else
-
     pointing_device_set_cpi(cursor_cpi_list[cursor_cpi_index]);
-
 #endif
 }
 
@@ -53,14 +55,10 @@ void trackball_cycle_cpi(void) {
     cursor_cpi_index = (cursor_cpi_index + 1) % 5;
 
 #ifdef POINTING_DEVICE_COMBINED
-
     pointing_device_set_cpi_on_side(true,  cursor_cpi_list[cursor_cpi_index]);
     pointing_device_set_cpi_on_side(false, cursor_cpi_list[cursor_cpi_index]);
-
 #else
-
     pointing_device_set_cpi(cursor_cpi_list[cursor_cpi_index]);
-
 #endif
 
     led_blink_times(cursor_cpi_index + 1);
@@ -72,27 +70,19 @@ void trackball_cycle_cpi(void) {
 void trackball_precision_mode(bool enable) {
 
 #ifdef POINTING_DEVICE_COMBINED
-
     if (enable) {
-
         pointing_device_set_cpi_on_side(true,  PRECISION_CPI);
         pointing_device_set_cpi_on_side(false, PRECISION_CPI);
-
     } else {
-
         pointing_device_set_cpi_on_side(true,  cursor_cpi_list[cursor_cpi_index]);
         pointing_device_set_cpi_on_side(false, cursor_cpi_list[cursor_cpi_index]);
-
     }
-
 #else
-
     if (enable) {
         pointing_device_set_cpi(PRECISION_CPI);
     } else {
         pointing_device_set_cpi(cursor_cpi_list[cursor_cpi_index]);
     }
-
 #endif
 }
 
@@ -100,17 +90,21 @@ void trackball_precision_mode(bool enable) {
 /* ---------- スクロール切替 ---------- */
 
 void trackball_scroll_toggle(void) {
-
     set_scrolling = !set_scrolling;
 }
 
 
-/* 左右役割反転(実装中) */
+/* ---------- 左右反転 ---------- */
 
 void trackball_swap_toggle(void) {
-
     swap_trackball = !swap_trackball;
+}
 
+
+/* ---------- ダブルカーソル ---------- */
+
+void trackball_dual_cursor_toggle(void) {
+    dual_cursor_mode = !dual_cursor_mode;
 }
 
 
@@ -123,11 +117,49 @@ report_mouse_t pointing_device_task_combined_user(
     report_mouse_t right_report
 ) {
 
-    /* ---------- 通常配置 ---------- */
+    /* ===== ダブルカーソル ===== */
+    if (dual_cursor_mode) {
+
+        /* 左トラボ補正（左右反転） */
+        left_report.x = -left_report.x;
+
+        if (set_scrolling) {
+
+            /* ===== 左スクロール ===== */
+            scroll_accumulated_h_l += (float)left_report.x / SCROLL_DIVISOR_H;
+            scroll_accumulated_v_l += (float)left_report.y / SCROLL_DIVISOR_V;
+
+            left_report.h = (int8_t)scroll_accumulated_h_l;
+            left_report.v = -(int8_t)scroll_accumulated_v_l;
+
+            scroll_accumulated_h_l -= (int8_t)scroll_accumulated_h_l;
+            scroll_accumulated_v_l -= (int8_t)scroll_accumulated_v_l;
+
+            left_report.x = 0;
+            left_report.y = 0;
+
+            /* ===== 右スクロール ===== */
+            scroll_accumulated_h_r += (float)right_report.x / SCROLL_DIVISOR_H;
+            scroll_accumulated_v_r += (float)right_report.y / SCROLL_DIVISOR_V;
+
+            right_report.h = (int8_t)scroll_accumulated_h_r;
+            right_report.v = -(int8_t)scroll_accumulated_v_r;
+
+            scroll_accumulated_h_r -= (int8_t)scroll_accumulated_h_r;
+            scroll_accumulated_v_r -= (int8_t)scroll_accumulated_v_r;
+
+            right_report.x = 0;
+            right_report.y = 0;
+        }
+
+        return pointing_device_combine_reports(left_report, right_report);
+    }
+
+
+    /* ===== 通常配置 ===== */
     if (!swap_trackball) {
 
         /* 左 → スクロール */
-
         scroll_accumulated_h += (float)left_report.x / SCROLL_DIVISOR_H;
         scroll_accumulated_v += (float)left_report.y / SCROLL_DIVISOR_V;
 
@@ -140,8 +172,7 @@ report_mouse_t pointing_device_task_combined_user(
         left_report.x = 0;
         left_report.y = 0;
 
-        /* 右 → カーソル or スクロール */
-
+        /* 右 */
         if (set_scrolling) {
 
             scroll_accumulated_h += (float)right_report.x / SCROLL_DIVISOR_H;
@@ -156,14 +187,12 @@ report_mouse_t pointing_device_task_combined_user(
             right_report.x = 0;
             right_report.y = 0;
         }
-
     }
 
-    /* ---------- 反転配置 ---------- */
+    /* ===== 反転配置 ===== */
     else {
 
         /* 右 → スクロール */
-
         scroll_accumulated_h += (float)right_report.x / SCROLL_DIVISOR_H;
         scroll_accumulated_v += (float)right_report.y / SCROLL_DIVISOR_V;
 
@@ -176,8 +205,7 @@ report_mouse_t pointing_device_task_combined_user(
         right_report.x = 0;
         right_report.y = 0;
 
-        /* 左 → カーソル or スクロール */
-
+        /* 左 */
         if (set_scrolling) {
 
             scroll_accumulated_h += (float)left_report.x / SCROLL_DIVISOR_H;
@@ -192,7 +220,6 @@ report_mouse_t pointing_device_task_combined_user(
             left_report.x = 0;
             left_report.y = 0;
         }
-
     }
 
     return pointing_device_combine_reports(left_report, right_report);
--- keymaps/vial/library/lib_trackball.h
+++ keymaps/vial/library/lib_trackball.h
@@ -7,3 +7,4 @@ void trackball_post_init(void);
 void trackball_cycle_cpi(void);
 void trackball_precision_mode(bool enable);
 void trackball_scroll_toggle(void);
+void trackball_dual_cursor_toggle(void);
